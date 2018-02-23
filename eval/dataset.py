7c7b8047b59c7712a51662f8479570ae488f1148
Lorenz Wellhausen <lorenwel@ethz.ch>
2018-02-23 21:49:02 +0100

    Added experimental files for working with own dataset

--- eval/dataset.py
+++ eval/dataset.py
@@ -19,7 +19,13 @@ def is_image(filename):
     return any(filename.endswith(ext) for ext in EXTENSIONS)
 
 def is_label(filename):
-    return filename.endswith("_labelTrainIds.png")
+    return any(filename.endswith(ext) for ext in EXTENSIONS)
+
+def is_self_supervised_image(filename):
+    return filename.endswith("_img.bmp")
+
+def is_self_supervised_label(filename):
+    return filename.endswith("_label.bmp")
 
 def image_path(root, basename, extension):
     return os.path.join(root, f'{basename}{extension}')
@@ -73,6 +79,48 @@ class cityscapes(Dataset):
 
         self.filenamesGt = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(self.labels_root)) for f in fn if is_label(f)]
         self.filenamesGt.sort()
+
+        self.input_transform = input_transform
+        self.target_transform = target_transform
+
+    def __getitem__(self, index):
+        filename = self.filenames[index]
+        filenameGt = self.filenamesGt[index]
+
+        #print(filename)
+
+        with open(image_path_city(self.images_root, filename), 'rb') as f:
+            image = load_image(f).convert('RGB')
+        with open(image_path_city(self.labels_root, filenameGt), 'rb') as f:
+            label = load_image(f).convert('P')
+
+        if self.input_transform is not None:
+            image = self.input_transform(image)
+        if self.target_transform is not None:
+            label = self.target_transform(label)
+
+        return image, label, filename, filenameGt
+
+    def __len__(self):
+        return len(self.filenames)
+
+
+class self_supervised_power(Dataset):
+
+    def __init__(self, root, input_transform=None, target_transform=None, subset='val'):
+        self.images_root = os.path.join(root, subset)
+        self.labels_root = os.path.join(root, subset)
+
+        print ("Image root is: " + self.images_root)
+        print ("Label root is: " + self.labels_root)
+
+        self.filenames = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(self.images_root), followlinks=True) for f in fn if is_self_supervised_image(f)]
+        self.filenames.sort()
+        print ("Found " + str(len(self.filenames)) + " images.")
+
+        self.filenamesGt = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(self.labels_root), followlinks=True) for f in fn if is_self_supervised_label(f)]
+        self.filenamesGt.sort()
+        print ("Found " + str(len(self.filenamesGt)) + " labels.")
 
         self.input_transform = input_transform
         self.target_transform = target_transform
