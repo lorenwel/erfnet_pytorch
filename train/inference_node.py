e806992fdfe9ca010443af43484136ab9486b320
Lorenz Wellhausen <lorenwel@ethz.ch>
2018-08-15 18:28:55 +0200

    Inference node cleanup

--- train/inference_node.py
+++ train/inference_node.py
@@ -10,12 +10,6 @@ import numpy as np
 import roslib
 import rospy
 from sensor_msgs.msg import Image
-# OpenCV
-import cv2
-from cv_bridge import CvBridge, CvBridgeError
-# PIL
-from PIL import Image as PILImage
-from torchvision.transforms import ToTensor
 
 
 
@@ -25,6 +19,8 @@ PUBLISHER_TOPIC= 'output/scalar_image_raw'
 
 
 class NetworkInferencer:
+
+
 
   def __init__(self, net, use_cuda):
     # Save net
@@ -36,10 +32,10 @@ class NetworkInferencer:
                                        Image, 
                                        self.callback, 
                                        queue_size=2)
-    # Create Cv Converter
-    self.bridge = CvBridge()
     # Listen for messages.
     rospy.spin()
+
+
 
   def do_inference(self, input):
 
@@ -60,6 +56,8 @@ class NetworkInferencer:
 
     return output
 
+
+
   def get_numpy_img(self, ros_img):
     self.shape = (ros_img.height, ros_img.width, 3)
     np_img = np.fromstring(ros_img.data, dtype=(np.uint8, 3))
@@ -67,19 +65,11 @@ class NetworkInferencer:
     return np_img
 
 
-  def callback(self, ros_img):
-    # Convert image to numpy
-    np_img = self.get_numpy_img(ros_img)
-    # Convert to Tensor
-    tensor_img = torch.Tensor(np_img)
 
-    # Do the inference
-    tensor_out = self.do_inference(tensor_img)
-
-    # Do output conversion
-    np_out = tensor_out.numpy()
+  def publish_output(self, np_out):
     # Convert to ROS image
     ros_out = Image(encoding="32FC1")
+    ros_out.header = ros_img.header
     ros_out.height = np_out.shape[0]
     ros_out.width = np_out.shape[1]
     contig = np.ascontiguousarray(np_out)
@@ -87,10 +77,20 @@ class NetworkInferencer:
     ros_out.step = contig.strides[0]
     # Publish
     self.val_pub.publish(ros_out)
-    
 
 
 
+  def callback(self, ros_img):
+    # Convert image to numpy
+    np_img = self.get_numpy_img(ros_img)
+    # Convert to Tensor
+    tensor_img = torch.Tensor(np_img)
+    # Do the inference
+    tensor_out = self.do_inference(tensor_img)
+    # Do output conversion
+    np_out = tensor_out.numpy()
+    # Publish
+    publish_output(npt_out)
 
 
 
